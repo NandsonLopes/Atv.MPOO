933c84543f01ada6e745e92a9888311f730284c2
NandsonLopes <167109474+NandsonLopes@users.noreply.github.com>
2024-04-17 19:27:46 -0300

    idprofessor.py

--- idprofessor.py
+++ idprofessor.py
@@ -1,18 +1,18 @@
 from idendereço import idendereço 
 
 class idprofessor (idendereço  ):
-    def __init__(self, name, street, number, zipCode):
-      self.name = name
-      super().__init__(street, number, zipCode)
+    def __init__(self, nome, rua, num, cep):
+      self.nome = nome
+      super().__init__(rua, num, cep)
 
-    def getName(self):
-       return self.name
+    def getnome(self):
+       return self.nome
     
-    def setName(self, newName):
-       self.name = newName
+    def setnome(self, novonome):
+       self.nome = novonome
 
     def __str__(self):
-       return f"name: {self.name}"
+       return f"nome: {self.nome}"
 
-professor = idprofessor("nandson", "00", "1", "5")
+professor = idprofessor("nandson", "00", "1", "59009898")
 print(professor)
